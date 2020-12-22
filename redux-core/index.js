b3178545036ab24a86e99b5dbbfe527615a5da5b
Heltzel <heltzel.service@hotmail.com>
2020-12-22 18:13:35 +0100

    Multiple reducers

--- redux-core/index.js
+++ redux-core/index.js
@@ -1,7 +1,9 @@
 const redux = require('redux')
 const createStore = redux.createStore
+const combineReducers = redux.combineReducers
 
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 // action creator
 function buyCake() {
@@ -10,11 +12,19 @@ function buyCake() {
     info: 'first redux action',
   }
 }
+function buyIcecream() {
+  return {
+    type: BUY_ICECREAM,
+    info: 'first redux action',
+  }
+}
 
 // reducer
 //(previousState, action) => newState
-const initialState = { numOfCakes: 10 }
-const reducer = (state = initialState, action) => {
+const initialCakeState = { numOfCakes: 10 }
+const initialIcecreamState = { numOfIceCreams: 20 }
+
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return { ...state, numOfCakes: state.numOfCakes - 1 }
@@ -22,14 +32,27 @@ const reducer = (state = initialState, action) => {
       return state
   }
 }
+const icecreamReducer = (state = initialIcecreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return { ...state, numOfIceCreams: state.numOfIceCreams - 1 }
+    default:
+      return state
+  }
+}
 
 // Store
-const store = createStore(reducer)
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCreame: icecreamReducer,
+})
+
+const store = createStore(rootReducer)
 console.log('initial state', store.getState())
 const unSubscribe = store.subscribe(() =>
   console.log('update State', store.getState()),
 )
 store.dispatch(buyCake())
 store.dispatch(buyCake())
-store.dispatch(buyCake())
+store.dispatch(buyIcecream())
 unSubscribe()
